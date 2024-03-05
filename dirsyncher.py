a972a1194c29b7b4669f6711652ee1b2368b12a9
Daniel Weber <daniel.weber@cispa.de>
2024-03-05 16:41:45 +0100

    add dir if it doesnt exist

--- dirsyncher.py
+++ dirsyncher.py
@@ -185,7 +185,7 @@ def parse_arguments() -> dict:
 
     
 def create_sshfs_mount(local_path, remote_path, remote_host):
-    p = subprocess.run(["sshfs", f"{remote_host}:{remote_path} 2>/dev/null", local_path])
+    p = subprocess.run(["sshfs", f"{remote_host}:{remote_path}", local_path])
 
     if p.returncode != 0:
         # probably the directory does not exist, hence we just try to create it.
